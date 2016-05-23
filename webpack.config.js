aa324d3c38df0ed2e06cf89a935431663b67a305
Hubert Zub <hubert.zub@softwarehut.pl>
2016-05-23 19:22:20 +0200

    Compressing js now

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,14 @@
+var webpack = require('webpack');
+
 module.exports = {
     context: __dirname + "/app",
     entry: "./main",
     output: {
         path: __dirname + "/dist",
         filename: "[name].js"
-    }
+    },
+    plugins: [
+        new webpack.optimize.UglifyJsPlugin()
+    ]
+
 };
